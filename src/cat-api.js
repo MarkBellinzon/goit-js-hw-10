ef58ddfce30e65084f3c5b0a142e46b2fe534a5b
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-18 21:20:13 +0300

    dz103

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,27 @@
-const url = 'https://api.thecatapi.com/v1';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_ora2yenLsClLubC7y9jHRX2GNIKNmEQ4tjX4Z6DOmTttdgrVgs4IExV6MTPj6mPp';
 
+//api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&key=live_ora2yenLsClLubC7y9jHRX2GNIKNmEQ4tjX4Z6DOmTttdgrVgs4IExV6MTPj6mPp
 function fetchBreeds() {
-  return fetch('${url}/breeds?api_key=${API_KEY}').then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
+  return fetch(
+    `${BASE_URL}/breeds?api_key=${API_KEY}&origin&wikipedia_url&life_span`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
     }
-    return res.json();
+    return resp.json();
   });
 }
 
 function fetchCatByBreed(breedId) {
   return fetch(
-    '${url}/images/search?api_key=${API_KEY}&breed_ids=${breedId}'
-  ).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}&origin&wikipedia_url&life_span`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
     }
-    return res.json();
+    return resp.json();
   });
 }
-
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,54 @@
 import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import './css/styles.css';
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
+breedSelect.addEventListener('change', onBreedSelect);
+
+function onBreedSelect(evt) {
+  const breedId = evt.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+      const { url, breeds, origin, wikipedia_url, life_span } = data[0];
+
+      catInfo.innerHTML = `<div class="box-img">
+      <img src="${url}" 
+      alt="${breeds[0].name}"
+      width="600"/>
+      </div>
+      <div class="box">
+      <h1>${breeds[0].name}</h1>
+      <p>${breeds[0].description}</p>
+      <p><b>Temperament:</b> ${breeds[0].temperament}</p>
+      <p><b>Country:</b> ${breeds[0].origin}</p>
+      <p><b>Age:</b> ${breeds[0].life_span}"</p>
+      <p><b>Wikipedia:</b>   <a href="${breeds[0].wikipedia_url}">wiki</a></p>
+      </div>`;
+      catInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+
+let arrBreedsId = [];
+fetchBreeds()
+  .then(data => {
+    data.forEach(element => {
+      arrBreedsId.push({ text: element.name, value: element.id });
+    });
+    new SlimSelect({
+      select: breedSelect,
+      data: arrBreedsId,
+    });
+  })
+  .catch(onFetchError);
+
+function onFetchError(error) {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+  );
+}
